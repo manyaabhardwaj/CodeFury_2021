e75eeb201658b53ca514fcd0fee356887b2af77b
srividyaghanta534 <srividyaghanta99@gmail.com>
2021-09-21 17:22:06 +0530

    main

---
+++ OnlineAuction/src/com/hsbc/onlineauction/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.hsbc.onlineauction.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println(username);
+		System.out.println(password);
+		PrintWriter out = resp.getWriter();
+		out.println("username is: "+username+" Password is: "+password);
+		out.close();
+	}
+}
