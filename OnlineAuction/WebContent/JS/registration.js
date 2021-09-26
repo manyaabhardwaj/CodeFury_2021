5a32af0f228257645e5d37383a09d7d2a688c2eb
Manya Bhardwaj <36292539+manyaabhardwaj@users.noreply.github.com>
2021-09-26 16:46:45 +0530

    UPDATE

--- OnlineAuction/WebContent/JS/registration.js
+++ OnlineAuction/WebContent/JS/registration.js
@@ -2,15 +2,15 @@ function validateName(){
 	var name = document.getElementById('name').value;
 	if(name==""|| name==null)
 	{
-		document.getElementById('nameerror').innerHTML="name is MUST";
+		document.getElementById('nameerror').innerHTML="Name is a mandatory field!";
 		 
 		return false;
 	}if (!/^[a-zA-z]+([\s][a-zA-Z]+)*$/g.test(name)) {
-		document.getElementById('nameerror').innerHTML = "name should contain only alphabets!";
+		document.getElementById('nameerror').innerHTML = "Name should contain only alphabets!";
 		return false;
 	}
 	if(name.length < 6 || name.length > 15){
-		document.getElementById('nameerror').innerHTML="length must be between 6 to 15";
+		document.getElementById('nameerror').innerHTML="Length must be between 6 to 15!";
 		
 		return false;
 	}else{
@@ -27,13 +27,13 @@ function validateDOB(){
 console.log(dob.value)
 	if (dob == null || dob == "") {
         // console.log("entered if"+dob);
-		document.getElementById('doberror').innerHTML="Invalid Date of Birth ";
+		document.getElementById('doberror').innerHTML="Invalid Date of Birth!";
 		return false; 
 
 	}
 	if (Date.parse(dob)<18) {
         // console.log("entered if"+dob);
-		document.getElementById('doberror').innerHTML="less than 18 ";
+		document.getElementById('doberror').innerHTML="less than 18! ";
 		return false; 
 	}else{
 		console.log("entered dob else");
@@ -47,7 +47,7 @@ function validateEmail(){
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!filter.test(email)) {
-	document.getElementById('emailerror').innerHTML="Invalid Email address ";
+	document.getElementById('emailerror').innerHTML="Please enter a valid Email address! ";
 		return false; 
 	}else{
 				document.getElementById('emailerror').innerHTML="";
@@ -59,7 +59,7 @@ function validatePhNum(){
 	var pnum=document.getElementById('phoneNumber').value;
 	var num=/^\d{10}$/;
 	if(!num.test(pnum)){
-		document.getElementById('phonenumbererror').innerHTML="Invalid Phone Number ";
+		document.getElementById('phonenumbererror').innerHTML="Please enter a valid Phone Number! ";
 		return false; 
 	}else{
 				document.getElementById('phonenumbererror').innerHTML="";
@@ -85,7 +85,7 @@ function validatePassword(){
 	var password = document.getElementById("password").value;
 	console.log(password);
         if(password=="" ){
-	document.getElementById('passworderror').innerHTML="Password cant be empty ";
+	document.getElementById('passworderror').innerHTML="Password cant be empty !";
 		return false;
 }	
        else{
@@ -97,11 +97,11 @@ function validateConfPassword(){
 	   var confirmPassword = document.getElementById("confirmpassword").value;
 	   var password = document.getElementById("password").value;
         if (password != confirmPassword) {
-            document.getElementById('Confpasserror').innerHTML="Pass word doesn't matches' ";
+            document.getElementById('Confpasserror').innerHTML="Both Password doesn't match ";
 	return false; 
         }
         	if( confirmPassword==""){
-	document.getElementById('Confpasserror').innerHTML="Password cant be empty ";
+	document.getElementById('Confpasserror').innerHTML="Password cant be empty! ";
 		return false;
 
 }
@@ -129,7 +129,7 @@ function validateAmount(){
 	var amount=document.getElementById('walletamount').value;
 	var valid = /^[0-9]+$/;
 	if(!valid.test(amount)){
-		document.getElementById('walleterror').innerHTML="Invalid Amount ";
+		document.getElementById('walleterror').innerHTML="Invalid Amount !";
 		return false; 
 	}else{
 				document.getElementById('walleterror').innerHTML="";
--- OnlineAuction/WebContent/JS/seller.js
+++ OnlineAuction/WebContent/JS/seller.js
@@ -1,18 +1,10 @@
 let sortDirection = false;
 let productData = [
-<<<<<<< HEAD
-    { productname: 'Clock', highestCurrentBid: '5000', noOfBidders: '8', bidEndDate: '2021-09-23' },
-    { productname: 'Keyboard', highestCurrentBid: '5000', noOfBidders: '10', bidEndDate: '2021-09-28' },
-    { productname: 'Charger', highestCurrentBid: '1300', noOfBidders: '1', bidEndDate: '2021-09-22' },
-    { productname: 'Watch', highestCurrentBid: '40000', noOfBidders: '3', bidEndDate: '2021-09-20' },
-    { productname: 'Shoes', highestCurrentBid: '5500', noOfBidders: '2', bidEndDate: '2021-09-20' },
-=======
     { productname: 'Clock', highestCurrentBid: '500', noOfBidders: '8', bidEndDate: '2021-09-23', imageNumber: '1' }, // comes from json data java
     { productname: 'Keyboard', highestCurrentBid: '5000', noOfBidders: '10', bidEndDate: '2021-09-28', imageNumber: '2' },
     { productname: 'Charger', highestCurrentBid: '1300', noOfBidders: '1', bidEndDate: '2021-09-22', imageNumber: '3' },
     { productname: 'Watch', highestCurrentBid: '4000', noOfBidders: '3', bidEndDate: '2021-09-20', imageNumber: '4' },
     { productname: 'Shoes', highestCurrentBid: '5500', noOfBidders: '2', bidEndDate: '2021-09-20', imageNumber: '5' },
->>>>>>> 30a38a78a74ca10653b4be29a0f962624df935f0
 ];
 
 
@@ -25,17 +17,10 @@ function loadTableData(productData) {
     let dataHTML = "<table><tr>";
 
     for (let product of productData) {
-<<<<<<< HEAD
-        dataHTML += "<td> "+product.productname +"</td><td>"+product.highestCurrentBid+ "</td><td>" +product.noOfBidders+"</td><td>"+ product.bidEndDate +"</td><td>"+"Delete"+"</td></tr>";
-    }
-    dataHTML+="</tr></table>";
-=======
         dataHTML += "<td>" + product.productname + "</td><td>" + product.highestCurrentBid + "</td><td>" + product.noOfBidders + "</td><td>" + product.bidEndDate + "</td><td><img height='80px' width='80px' src='../tableImages/image" + product.imageNumber + ".jpg' /></td><td>" + "delete" + "</td></tr>";
     }
     dataHTML += "</tr></table>"
 
->>>>>>> 30a38a78a74ca10653b4be29a0f962624df935f0
     console.log(dataHTML)
     tableBody.innerHTML = dataHTML;
 }
-
